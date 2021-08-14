4016e96db6ebcb646038d47624c1b9507671251a
kangsunghan <kangsh411@naver.com>
2021-08-15 04:27:00 +0900

    commit

--- webapp/controller/MainList.controller.js
+++ webapp/controller/MainList.controller.js
@@ -1,68 +1,89 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/m/MessageBox",
-	'sap/m/MessageToast'
+	'sap/m/MessageToast',
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+	"sap/ui/model/json/JSONModel",
 ], function(
 	Controller, 
 	MessageBox,
-	MessageToast
+	MessageToast,
+	Filter,
+	FilterOperator,
+	JSONModel
 ) {
 	"use strict";
 
 	return Controller.extend("createsalesorder.controller.MainList", {
         onInit: function () {
-			this.getOwnerComponent().getRouter().getRoute("MainList").attachMatched(this.onRouter, this);
-		},
+			var oModel = new JSONModel({
+				create : {
+					mandt : '100',  
+					sono : '',
+					sodat : '',
+					cuscd : ''							
+				}
+				,filter : {
+					sono : '',
+					sodat : '',
+					cuscd : ''
+				},
+				it_data  : [],
+				it_data2 : [],
+				editMode : false
+			});
 
-		onRouter : function(){
-			this.getOwnerComponent().getModel("viewModel").setProperty("/park","조회클릭");
-
+			this.getView().setModel(oModel, "viewModel");
+		
 		},
 		onSearch: function (evt) {
-			MessageBox.show(this.getOwnerComponent().getModel("viewModel").getProperty("/order") + this.getOwnerComponent().getModel("viewModel").getProperty("/customer"));
+			// MessageBox.show(this.getOwnerComponent().getModel("viewModel").getProperty("/order") + this.getOwnerComponent().getModel("viewModel").getProperty("/customer"));
 
-			this.getModel().read({
+			// this.getModel().read({
 				
-			})
+			// })
+			
+
+			//////////////////////
+			var oModel = this.getView().getModel("viewModel");
+				var oEdit = oModel.setProperty('/editMode', false);
+						
+				var oFilter = oModel.getProperty('/filter');
+
+				var mFilter = [];
+				for(var sKey in oFilter){
+					var oVal = oFilter[sKey];
+					if(oVal) {
+						mFilter.push(new Filter({
+							path: sKey,
+							operator: FilterOperator.Contains,
+							value1: oVal,
+						}));
+					}
+				}
+
+				var oModelS = this.getOwnerComponent().getModel();
+				oModelS.read('/ZAA_SOSet', {
+					filters : mFilter,
+					success : function(oEvent){
+						var aData = []; 
+						if(oEvent.results.length > 0){
+							aData = JSON.parse(oEvent.results[0].it_data);
+							oModel.setProperty('/it_data', aData);
+						}	
+					},
+					error : function(oEvent){
+						MessageBox.show('Search Error!');
+					}
+				})
+				
 		},
 		onCreate: function() {
 			this.getOwnerComponent().getRouter().navTo("Object",{
 				orderNumber : "New"
 			});
 		},
-		onValueHelpRequested: function() {
-			var aCols = this.oColModel.getData().cols;
-
-			Fragment.load({
-				name: "createsalesorder.controller.MainList",
-				controller: this
-			}).then(function name(oFragment) {
-				this._oValueHelpDialog = sap.ui.xmlfragment("createsalesorder.controller.MainList", this);
-				this.getView().addDependent(this._oValueHelpDialog);
-
-				this._oValueHelpDialog.getTableAsync().then(function (oTable) {
-					oTable.setModel(this.oProductsModel);
-					oTable.setModel(this.oColModel, "columns");
-
-					if (oTable.bindRows) {
-						oTable.bindAggregation("rows", "/ProductCollection");
-					}
-
-					if (oTable.bindItems) {
-						oTable.bindAggregation("items", "/ProductCollection", function () {
-							return new ColumnListItem({
-								cells: aCols.map(function (column) {
-									return new Label({ text: "{" + column.template + "}" });
-								})
-							});
-						});
-					}
-					this._oValueHelpDialog.update();
-				}.bind(this));
-
-				this._oValueHelpDialog.setTokens(this._oMultiInput.getTokens());
-				this._oValueHelpDialog.open();
-			}.bind(this));
-		},
+		
 	});
 });
